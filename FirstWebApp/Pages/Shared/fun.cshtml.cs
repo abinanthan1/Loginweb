9688ffb7a8ac13a9689a439b234e7cd6611a0d65
abinanthan1 <abinanthan1011@gmail.com>
2023-07-29 12:20:36 +0530

    new page added

---
+++ FirstWebApp/Pages/Shared/fun.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstWebApp.Pages.Shared
+{
+    public class funModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
